9cd3c64b75d8218ede4c80ebe9d1d92f8c3f11c5
simon-off <hellosimonoff@gmail.com>
2023-04-01 12:13:15 +0200

    Added line-up info

--- js/line-up.js
+++ js/line-up.js
@@ -2,88 +2,117 @@ const lineUp = [
   {
     name: "Tiger & Träden",
     shortDescription:
-      "Tiger & Träden sjunger om livet och kärleken. Melodierna och rytmerna är själens flykt från kroppen.",
-    longDescription: `Bakom Tiger & Trädens låtar står Jonathan Lundén, en lantpojk från Skaraslätten som fångats av musiken. Vid pianot i sitt hus uttrycker Jonathan sina upplevelser av livet och i studion på ovanvåningen i samma hus fångas ljudvågorna in och produceras till låtar som läggs ihop och blir album. 
-
-    2020 kom debutplattan "Jag är här nu" och under 2023 släpps dubbeluppföljaren Små låtar CD 1 och 2. 
+      "Sjunger om livet och kärleken. Melodierna och rytmerna är själens flykt från kroppen.",
+    longDescription: `Bakom Tiger & Trädens låtar står Jonathan Lundén, en lantpojk från Skaraslätten som fångats av musiken. Vid pianot i sitt hus uttrycker Jonathan sina upplevelser av livet och i studion på ovanvåningen i samma hus fångas ljudvågorna in och produceras till låtar som läggs ihop och blir album.
+    
+    2020 kom debutplattan "Jag är här nu" och under 2023 släpps dubbeluppföljaren Små låtar CD 1 och 2.
     
     Musiken är fri och texterna ärliga, samtidigt allvarsamma som lättsamma. På Folk från skogen bjuder Tiger & Träden, backad av ett band av högsta världsklass, in er att bada i sin själ. Låtar kommer låta och känslor kommer kännas.`,
     img: "tiger.jpg",
     instagramUrl: "https://www.instagram.com/tigerochtraden/",
-    spotifyUrl: "#",
+    spotifyUrl: "https://open.spotify.com/artist/0DTpsTLFViygxRyO94uDql",
   },
   {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
+    name: "Effy Simon",
+    shortDescription:
+      "Med träffsäkra melodier och en jazzinfluerad ljudbild tar Effy Simon med er på en resa tillbaka i tiden då vispopen var som bäst.",
+    longDescription: `Låtskrivaren Effy Simon fann kärleken till de varma akustiska tonerna vid 10 års ålder, då hon fick sin första ukulele av sin farmor. Från att tidigare ha skrivit låtar på engelska har den svenska vispopen fått ta en allt större plats i hennes musik.
+
+    Idag lever de själfulla klangerna vidare i hennes melodiska vispop, som bjuder på lättsamma med glimten i ögat.
+    Nu är hon redo för att möta och dela sin musik på hemmaplan med låtar från hennes kommande EP 'Känslosam' som släpps under våren 2023.`,
     img: "effy-simon.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
+    instagramUrl: "https://www.instagram.com/effysimonmusic/",
+    spotifyUrl: "https://open.spotify.com/artist/0aAHKgdpJDLjJw2vWYsRA0",
   },
   {
     name: "Wilhelm",
-    shortDescription: "",
+    shortDescription:
+      "Lokal förmåga med en pipa som heter duga. Slö folkmusik med texter skrivna ur en gammal mans perspektiv.",
     longDescription: ``,
     img: "wilhelm.jpg",
-    instagramUrl: "#",
+    instagramUrl: "https://www.instagram.com/vvllhelm/",
     spotifyUrl: "#",
   },
   {
-    name: "Wilhelm",
-    shortDescription: "",
+    name: "Wilma Holmes",
+    shortDescription:
+      "Med en röst som doftar urskog, och ett musikaliskt landskap som varsamt skiftar mellan mörker och ljus, träffar Wilma Holmes musik rakt in i själen.",
     longDescription: ``,
     img: "wilma-holmes.jpg",
-    instagramUrl: "#",
+    instagramUrl: "https://www.instagram.com/wilmaaholmes/",
     spotifyUrl: "#",
   },
   {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
+    name: "Eternal Wonder",
+    shortDescription:
+      "Psytrance-producent och DJ från Torsås. Hans musik kan beskrivas som elektronisk med hög energi och psykadeliska sköna vibbar.",
+    longDescription: `Eternal Wonder (Tidigare Empty Wheelchair) är Albin Appelqvist, en Psytrance-producent och DJ från Torsås. Albin började spela gitarr men upptäckte senare elektronisk musik och började producera egen musik under tonåren. 
+    
+    År 2011 Släppte han sin första EP - The Countdown och har haft flera släpp på olika bolag genom åren, bland annat DNA Records (Israel), Zombster Records (Italien) och sitt egna skivbolag, Unimuse Records där han jobbar med artister i från hela världen. Han Har spelat på flera olika festivaler och Underground-event i både norra och södra Sverige, Några av dem är Live Green Festival (Sedan starten 2011), Månsken-festen 2016 (Stockhom), Forest Star Festival 2017 (Hjo) och Kalmar Stadsfest 2017. 
+    
+    Hans musik kan beskrivas som elektronisk musik med hög energi och psykadeliska sköna vibbar. Hans andra album - Faithful Daydreamer släpptes i Juli 2022`,
     img: "eternal-wonder.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
+    instagramUrl: "https://www.instagram.com/eternalwonderofficial/",
+    spotifyUrl: "https://open.spotify.com/artist/2xwC2G9ccAaNFqzebuwszJ",
   },
   {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
+    name: "One Legged Gardener",
+    shortDescription:
+      "Ur de värmländska skogarna träder One Legged Gardener fram med texter som känns, omslutna av detaljrika gitarrarrangemang och ambienta ljudlandskap.",
+    longDescription: `One legged gardener består av den Örebrobaserad värmlänningen Otto Hedin. Med texter som berör destruktiva relationer och psykisk ohälsa vill han öppna upp för samtal om det som lätt hamnar i skymundan.
+    
+    Paketerat i folkiga gitarrplock och ambienta klanger där det ljusa får möta det mörka bjuder One Legged Gardener in till en kontrastrik musikupplevelse. På den första av många festivaler arrangerad av Folk Från Skogen bjuder One Legged Gardener på både nytt och gammalt material, vi ses i sommar!`,
     img: "one-legged-gardener.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
+    instagramUrl: "https://www.instagram.com/oneleggedgardener/",
+    spotifyUrl: "https://open.spotify.com/artist/28h03UnzdUxVNI7FDyfJZu",
   },
   {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
-    img: "rain-stall.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
-  },
-  {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
-    img: "warm-winter.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
-  },
-  {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
-    img: "elin-ottilia.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
-  },
+    name: "Rain Stall",
+    shortDescription:
+      "Rain Stall sträcker sig efter den intimitet som kliar och vars smak skär sig.",
+    longDescription: `Ur den skånska glesbygdens industriella idyll uppstod musikprojektet Rain Stall genom Alec Rembratts utforskande av intima uttryck som lämnar en besk smak i munnen.
 
+    Med influenser från drone, slowcore och folk, kan musiken beskrivas som dunkel lo-fi folk, insmord i atmosfärisk blues och astrologisk reverb. Gitarrer som tycks spelas från en gammal radio, ackompanjeras med en viskande sång och suddiga väsen som berättar en historia om någon som ser ut att vara påväg någonstans farligt viktigt. Bandet består utöver Rembratt, av Rasmus Svensson, Pebbles Hörberg och Joel Bengtsson.
+
+    2019 släpptes debutalbumet ‘’A place to meet and talk’’ och monikerns senast sammanställda verk är EPn ‘’Ride’’, som släpptes 2022. Senare i år väntas Rain Stall släppa sitt andra album som består av 10 låtar.`,
+    img: "rain-stall.jpg",
+    instagramUrl: "https://www.instagram.com/dozingdreamflock/",
+    spotifyUrl: "https://open.spotify.com/artist/6Opnw1yVHAuqSwziQdqnAd",
+  },
   {
-    name: "Wilhelm",
-    shortDescription: "",
-    longDescription: ``,
+    name: "Warm Winter",
+    shortDescription:
+      "Med en blandning av 60-talets galenskap och Indiefolkens dysterhet letar Warm Winter ständigt efter balansen mellan varmt och kallt.",
+    longDescription: `Warm Winter berättar i realtid historien om hans inre värld, messiaskomplexet, det vardagliga förortslivet, den övernaturliga månskensdränkta meningslösheten och resan till döden. Efter det senaste EP-släppet Bones ‘n’ Flesh gjorde Warm Winter & the Warm Intentions en rad hyllade debut spelningar på Stockholms Indiescen med en avslutande konsert på den anrika klubben Trädgården (Huset Under Bron).
+
+    TLDR: Med några enkla ackord, stadigt höftsväng och taskig falsett skapar denna karaktär en Stämning. 
+
+    Kom får du se… <3`,
+    img: "warm-winter.jpg",
+    instagramUrl: "https://www.instagram.com/warmwinterwarm/",
+    spotifyUrl: "https://open.spotify.com/artist/4yivpq7eOVfPYU3rzIBvvv",
+  },
+  {
+    name: "Elin Ottilia",
+    shortDescription:
+      "Med sina rötter i indie pop/rock-scenen skriver Elin Ottilia låtar som blandar attityd, sårbarhet och närvaro.",
+    longDescription: `Med sina rötter i indie pop/rock-scenen och med influenser som Beabadoobee och Phoebe Bridgers är Elin Ottilia precis det den Svenska indiescenen behöver. Låtarna hon skriver blandar attityd, sårbarhet och närvaro.
+
+    Elin Ottilia är aktuell med singeln mind/head som släpptes i januari 2023 och under våren kommer två till singlar att släppas och slutligen kommer en debut-EP i maj. Det här är helt enkelt bara början!`,
+    img: "elin-ottilia.jpg",
+    instagramUrl: "https://www.instagram.com/elin.ottilias/",
+    spotifyUrl: "https://open.spotify.com/artist/4L62RXDf8a70pfoQ9iWYkK",
+  },
+  {
+    name: "Marcus Ekdahl",
+    shortDescription:
+      "En singer/songwriter som skriver låtar om främst sakerna i livet som inte alltid är enkla att prata om men även om fina barndomsminnen. ",
+    longDescription: `När den första låten skrevs för 10 år sedan i 2013 så var den stora drömmen att få vara frontman i ett coolt indieband och sjunga på engelska för att nå ut till en så stor publik som möjligt. Något band blev det inte men låtar blev skrivna och engelska var språket som blev det naturliga att skriva låttexter på. 
+    
+    Nu har åren gått och äntligen har Marcus Ekdahl hittat det fina med att skriva på sitt modersmål och har under den senaste tiden skrivit mycket på svenska. Låtar han håller nära hjärtat och där den rena ärligheten är stommen som håller upp låtarna. Låtar som handlar om familjedrama, hjärtesorg, existensiella frågor och fina minnen från barndomen.`,
     img: "marcus.jpg",
-    instagramUrl: "#",
-    spotifyUrl: "#",
+    instagramUrl: "https://www.instagram.com/marcusekdahl/",
+    spotifyUrl: "https://open.spotify.com/artist/5g7jYFCJjxBHv9To3OKxcx",
   },
 ];
 
