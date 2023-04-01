319990ab02610d637ee3c5c29a435d07b6dd982d
simon-off <hellosimonoff@gmail.com>
2023-04-01 12:13:34 +0200

    added phone obfuscation

--- js/mail.js
+++ js/mail.js
@@ -1,7 +1,13 @@
+const mlink = document.getElementById("mlink");
+const pnumber = document.getElementById("pnumber");
+
 const me = "folkfranskogen";
 const place = "gmail.com";
-
-const mlink = document.getElementById("mlink");
+const phone1 = "+46";
+const phone2 = 733427860;
 
 mlink.href = `mailto:${me}@${place}`;
 mlink.innerText = `${me}@${place}`;
+
+pnumber.href = `tel:${phone1}${phone2 + 1}`;
+pnumber.innerText = `073 342 78 61`;
