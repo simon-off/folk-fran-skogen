6948772fede70b1dc93682c9e8463500389c8a40
simon-off <simon-off@users.noreply.github.com>
2024-01-27 14:42:28 +0100

    Remove phone number

--- js/mail.js
+++ js/mail.js
@@ -1,13 +1,7 @@
 const mlink = document.getElementById("mlink");
-const pnumber = document.getElementById("pnumber");
 
 const me = "kontakt";
 const place = "folkfranskogen.se";
-const phone1 = "+46";
-const phone2 = 733427860;
 
 mlink.href = `mailto:${me}@${place}`;
 mlink.innerText = `${me}@${place}`;
-
-pnumber.href = `tel:${phone1}${phone2 + 1}`;
-pnumber.innerText = `073 342 78 61`;
