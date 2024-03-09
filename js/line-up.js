22e85233bd7c48fc93b34649ac9ac8056f556b45
simon-off <simon-off@users.noreply.github.com>
2024-03-09 11:42:50 +0100

    Add new artist to line-up

--- js/line-up.js
+++ js/line-up.js
@@ -8,6 +8,15 @@ const lineUp = [
     instagramUrl: "https://www.instagram.com/svalarna/",
     spotifyUrl: "https://open.spotify.com/artist/3Ccd7Fr6wNkGfifwE4rm1m",
   },
+  {
+    name: "GRIM",
+    description: `GRIM är artistnamnet för låtskrivaren och sångaren Alvin Blomberg. Den första mars släpptes hans självbetitlade debutalbum. 
+
+      Musiken har jämförts med 60 - och 70-talens berättelsedrivna singer-songwriters som Bob Dylan och Nick Drake, men anslaget är dock Alvins eget. Det karaktäriseras av ett utvecklat fingerspel på gitarren och stämningsfulla folksånger där texten och de starka melodierna står i centrum.`,
+    img: "grim.jpg",
+    instagramUrl: "https://www.instagram.com/grim.music/",
+    spotifyUrl: "https://open.spotify.com/artist/0MokmTSYVP7JttFUbXX6W7",
+  },
 ];
 
 export default lineUp;
