e65ffd10ccf58ac3b463a436413af96af90f056c
simon-off <simon-off@users.noreply.github.com>
2024-03-09 11:45:20 +0100

    Change order of line-up

--- js/line-up.js
+++ js/line-up.js
@@ -1,14 +1,5 @@
 const lineUp = [
   {
-    name: "Svalarna",
-    description: `Svalarna bjuder in till en värld där fascinationen för det övernaturliga möter ömtåligheten av ett helt vanligt människohjärta. 
-  
-      I sommar samlas en brokig skara musiker från olika bakgrund där jazz, folk och klassisk musik skapar en genreöverskridande atmosfär. Dröm fritt och låt Svalarna leda dig till något oupptäckt.`,
-    img: "svalarna.jpg",
-    instagramUrl: "https://www.instagram.com/svalarna/",
-    spotifyUrl: "https://open.spotify.com/artist/3Ccd7Fr6wNkGfifwE4rm1m",
-  },
-  {
     name: "GRIM",
     description: `GRIM är artistnamnet för låtskrivaren och sångaren Alvin Blomberg. Den första mars släpptes hans självbetitlade debutalbum. 
 
@@ -16,6 +7,15 @@ const lineUp = [
     img: "grim.jpg",
     instagramUrl: "https://www.instagram.com/grim.music/",
     spotifyUrl: "https://open.spotify.com/artist/0MokmTSYVP7JttFUbXX6W7",
+  },
+  {
+    name: "Svalarna",
+    description: `Svalarna bjuder in till en värld där fascinationen för det övernaturliga möter ömtåligheten av ett helt vanligt människohjärta. 
+  
+      I sommar samlas en brokig skara musiker från olika bakgrund där jazz, folk och klassisk musik skapar en genreöverskridande atmosfär. Dröm fritt och låt Svalarna leda dig till något oupptäckt.`,
+    img: "svalarna.jpg",
+    instagramUrl: "https://www.instagram.com/svalarna/",
+    spotifyUrl: "https://open.spotify.com/artist/3Ccd7Fr6wNkGfifwE4rm1m",
   },
 ];
 
