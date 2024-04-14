02261fabb589ea12d9784d2b69cbc68c6cea1c40
simon-off <simon-off@users.noreply.github.com>
2024-04-14 10:07:22 +0200

    Add artist to line-up

--- js/line-up.js
+++ js/line-up.js
@@ -1,9 +1,9 @@
 const lineUp = [
   {
     name: "VILMA FLOOD",
-    description: `Sedan debutalbumet ”Moodswinger”, som hyllades unisont av svensk press 2019, har Vilma Flood setts flitigt turnerande, med höjdpunkter som ett utsålt Fasching i Stockholm, Dalhalla och finsmakarfestivalen Sthlm Americana, samt som gäst till Moonica Mac ihop med Sara Parkman i Så Mycket Bättre.
+    description: `Sedan debutalbumet “Moodswinger”, som hyllades unisont av svensk press 2019, har Vilma Flood setts flitigt turnerande, med höjdpunkter som ett utsålt Fasching i Stockholm, Dalhalla och finsmakarfestivalen Sthlm Americana, samt som gäst till Moonica Mac ihop med Sara Parkman i Så Mycket Bättre.
   
-      Till Gullabo kommer årets headline-akt Vilma Flood med fullband, ett episkt avslut på festivalen. `,
+      Till Gullabo kommer årets headline-akt Vilma Flood med fullband, ett episkt avslut på festivalen.`,
     img: "vilma-flood.jpg",
     instagramUrl: "https://www.instagram.com/vilmaflood/",
     spotifyUrl:
@@ -11,7 +11,7 @@ const lineUp = [
   },
   {
     name: "DALMATIN",
-    description: `I höstas slog Sveriges nya pop-sensation Dalmatin ned som en bomb från ingenstans. Trion inledde 2024 med att släppa debut-EP:n ’’Dagarna & kvällarna’’, nomineras till ’’Årets genombrott’’ i Gaffa-priset och rotera på P3 med låten ’’Festival’’.
+    description: `I höstas slog Sveriges nya pop-sensation Dalmatin ned som en bomb från ingenstans. Trion inledde 2024 med att släppa debut-EP:n “Dagarna & kvällarna”, nomineras till Årets genombrott i Gaffa-priset och rotera på P3 med låten “Festival”.
 
       I vår kommer debutalbumet och sommarturnén är ett faktum. Bli en av dom som såg Dalmatin första gången i Småland.`,
     img: "dalmatin.jpg",
@@ -29,7 +29,7 @@ const lineUp = [
   },
   {
     name: "WILMA HOLMES",
-    description: `Wilma Holmes karaktäristiska röst som rotar sig i kulning och popmusikens melodikänsla, är nu redo att höras över hela världen. Debutalbumet 'Intruder' släpps i maj, en debut som tydligt porträtterar Holmes obevekliga nyfikenhet, och som vandrar från det mörkaste mörkret till ljuset i slutet av tunneln. 
+    description: `Wilma Holmes karaktäristiska röst som rotar sig i kulning och popmusikens melodikänsla, är nu redo att höras över hela världen. Debutalbumet “Intruder” släpps i maj, en debut som tydligt porträtterar Holmes obevekliga nyfikenhet, och som vandrar från det mörkaste mörkret till ljuset i slutet av tunneln. 
   
       I sommar åker hon ut på release-turné och gör sin allra första festivalspelning hos oss i Gullabo.`,
     img: "wilma-holmes.jpg",
@@ -44,6 +44,17 @@ const lineUp = [
     img: "svalarna.jpg",
     instagramUrl: "https://www.instagram.com/svalarna/",
     spotifyUrl: "https://open.spotify.com/artist/3Ccd7Fr6wNkGfifwE4rm1m",
+  },
+  {
+    name: "ISABEL NEIB",
+    description: `Isabel Neib är artisten och producenten från Sundsvall som släppte sitt hyllade debutalbum “I Already Know” 24 November 2023. GAFFA skriver: “Isabel Neib kliver fram som artist i sin egen rätt. Resultatet är en väl slipad och personlig resa.” 
+    
+      Hennes musik kan beskrivas som kompromisslös, melankolisk och hoppfull elektronisk pop i dess bästa form. Med en brinnande passion och oändlig ambition gick vägen från turnerande musiker, och samarbetspartner åt exempelvis Iiris Viljanen och Jonathan Johansson, till solokarriären.
+  
+      I sommar får vi äran att höra Isabel Neib i skogen och hon har med sig sina medmusiker Agnes Wallmark på keys (känd från Svenska Akademien) samt Johan Sigerud på Ableton Push.`,
+    img: "isabel-neib.jpg",
+    instagramUrl: "https://www.instagram.com/isabelneib/",
+    spotifyUrl: "https://open.spotify.com/artist/7hsUdPSrlTdl9cHyGl7KL5",
   },
 ];
 
