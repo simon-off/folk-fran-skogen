4bca77d1760fd534fee29ad918e7352780909952
simon-off <simon-off@users.noreply.github.com>
2025-01-12 14:48:13 +0100

    Disable overwrite on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  // TODO: Uppdatera script så man tar bort alla gamla genererade filer
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: `assets/[name].js`,
+  //       chunkFileNames: `assets/[name].js`,
+  //       assetFileNames: `assets/[name].[ext]`
+  //     }
+  //   }
+  // }
 })
