038bfcd18f826968ae475d134095a9569d38e359
simon-off <simon-off@users.noreply.github.com>
2025-02-16 18:46:14 +0100

    Update deployment script

--- deploy.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Building site..."
-pnpm run build
-cp -r dist public_html
-
-Write-Host "Copying files to server..."
-scp -r public_html folkfranskogen:~/
-rm -r public_html
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  // TODO: Uppdatera script så man tar bort alla gamla genererade filer
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       entryFileNames: `assets/[name].js`,
-  //       chunkFileNames: `assets/[name].js`,
-  //       assetFileNames: `assets/[name].[ext]`
-  //     }
-  //   }
-  // }
-})
+export default defineConfig({})
